0cb64becf22b9a3ff974a32eeca8306344c7679d
Zhen715code <118883154+Zhen715code@users.noreply.github.com>
2022-11-25 12:59:58 +0800

    Update main_Epilepsy.m

--- main_Epilepsy.m
+++ main_Epilepsy.m
@@ -40,68 +40,10 @@ for index =1:n_sub
     EEGdata((index-1)*dc+1:index*dc,:)=Y;
 end
 L=W*Gain;
-
-wMNE_s_group=zeros(n_sub,ds,dt);
-LORETA_s_group=zeros(n_sub,ds,dt);
-L2L2L2_s_group=zeros(n_sub,ds,dt);
-SBL_s_group=zeros(n_sub,ds,dt);
-GL_s_group=zeros(n_sub,ds,dt);
-Dirty_s_group=zeros(n_sub,ds,dt);
-MTW_s_group=zeros(n_sub,ds,dt);
 MTWVSSI_s_group=zeros(15,n_sub,ds,dt);
- 
-wMNE_ev=zeros(1,n_sub);
-LORETA_ev=zeros(1,n_sub);
-L2L2L2_ev=zeros(1,n_sub);
-SBL_ev=zeros(1,n_sub);
-GL_ev=zeros(1,n_sub);
-Dirty_ev=zeros(1,n_sub);
-MTW_ev=zeros(1,n_sub);
 MTWVSSI_ev=zeros(1,n_sub);
-
-wMNE_thr=zeros(1,n_sub);
-LORETA_thr=zeros(1,n_sub);
-L2L2L2_thr=zeros(1,n_sub);
-SBL_thr=zeros(1,n_sub);
-GL_thr=zeros(1,n_sub);
-Dirty_thr=zeros(1,n_sub);
-MTW_thr=zeros(1,n_sub);
 MTWVSSI_thr=zeros(1,n_sub);
 MTWVSSI_m=zeros(15,n_sub,ds);
-
-for sub=2:n_sub
-    Y=EEGdata((sub-1)*dc+1:sub*dc,:);
-    %  wMNE
-    [Kernel,par] = MNE(Y,Gain3,L,Cortex,'wMNE','reg',1);
-    S_wMNE = Kernel*Y*ratio;
-    wMNE_s_group(sub,:,:)=S_wMNE;
-    wMNE_thr(1,sub)=ThresholdSelect(S_wMNE, 'ifplot',0);
-    wMNE_ev(1,sub)= 1 - norm(Y - L*S_wMNE./ratio,'fro')^2/norm(Y,'fro')^2;
-    
-    %  LORETA
-    [Kernel,par] = MNE(Y,Gain3,L,Cortex,'LORETA','reg',1);
-    S_LORETA = Kernel*Y*ratio;
-    LORETA_s_group(sub,:,:)=S_LORETA;
-    LORETA_thr(1,sub)=ThresholdSelect(S_LORETA, 'ifplot',0);
-    LORETA_ev(1,sub)=1 - norm(Y - L*S_LORETA./ratio,'fro')^2/norm(Y,'fro')^2;
-    
-    %  SBL
-    [Kernel,par] = SBL(Y,L,'epsilon',1e-4,'flags',1,'prune',[1,1e-6]);
-    S_SBL = Kernel*Y*ratio;
-    SBL_s_group(sub,:,:)=S_SBL;
-    SBL_thr(1,sub)=ThresholdSelect(S_SBL, 'ifplot',0);
-    SBL_ev(1,sub)=1 - norm(Y - L*S_SBL./ratio,'fro')^2/norm(Y,'fro')^2;
-    
-    %%  SISSY_L21
-    sigma1=0.01*norm(Y,1)/sqrt(dc);
-    lamb=1;
-    mu=5; 
-    [S_L2L2L2,R] = SISSY_L21(L, Y,VertConn,lamb*mu*sigma1,lamb,1e3,1e3,1.5);%80,0.1  (L, b, v,  lambda1,lambda2,rho1,rho2,alpha)
-     S_L2L2L2=S_L2L2L2*ratio;
-    L2L2L2_s_group(sub,:,:)=S_L2L2L2;
-    L2L2L2_thr(1,sub)=ThresholdSelect(S_L2L2L2, 'ifplot',0);
-     L2L2L2_ev(1,sub)=1 - norm(Y - L*S_L2L2L2./ratio,'fro')^2/norm(Y,'fro')^2;
-end
 
 %%  SVD for TBFs
 Y_all2=EEGdata;
@@ -109,91 +51,10 @@ Y_all2=EEGdata;
 Y_DIC=Y_all2*Dic';
 n_dic=size(Dic,1);
 [~,ds] = size(Gain);
-%%  GL 
-fprintf('%s\n','-----------GL-------------');
-S_GL=zeros(n_dic,ds,n_sub);
-lambda=1e-3;
-rho=1e2;
-for t=1:n_dic
-    Y2=zeros(dc,n_sub);
-    for sub=1:n_sub
-        Y1=Y_DIC(:,t);
-        Y2(:,sub)=Y1((sub-1)*dc+1:sub*dc,:);
-    end
-    S_GL(t,:,:) =GL(L, Y2,lambda,rho,1.8);
-end
-for sub=1:n_sub
-    s_GL=S_GL(:,:,sub)'*Dic*ratio;
-    GL_s_group(sub,:,:)=s_GL;
-    GL_thr(1,sub)=ThresholdSelect(s_GL, 'ifplot',0);
-    Y=EEGdata((sub-1)*dc+1:sub*dc,:)*ratio;
-    GL_ev(1,sub)=1 - norm(Y - L*s_GL,'fro')^2/norm(Y,'fro')^2;
-end
-
-%%  Dirty
-fprintf('%s\n','-----------Dirty--------------');
-S_Dirty=zeros(n_dic,ds,n_sub);
-alpha=10;
-beta=8;
-for t=1:n_dic
-    Y2=zeros(dc,n_sub);
-    for sub=1:n_sub
-        Y1=Y_DIC(:,t);
-        Y2(:,sub)=Y1((sub-1)*dc+1:sub*dc,:);
-    end
-    S_Dirty(t,:,:) = Dirty(Y2',L, alpha,beta);
-end
-for sub=1:n_sub
-    s_dirty=S_Dirty(:,:,sub)'*Dic*ratio;
-    Dirty_s_group(sub,:,:)=s_dirty;
-    Dirty_thr(1,sub)=ThresholdSelect(s_dirty, 'ifplot',0);
-    Y=EEGdata((sub-1)*dc+1:sub*dc,:)*ratio;
-   Dirty_ev(1,sub)=1 - norm(Y - L*s_dirty,'fro')^2/norm(Y,'fro')^2;
-end
-
-%%  MTW
-fprintf('%s\n','-----------MTW--------------');
 M = D/median(reshape(D,1,[]));
 epsilon=100. /ds;
 gamma=max(max(M));
 M=(- M / epsilon);
-MTW_lamb=1e1;
-MTW_mu=1e1;
-tol=5;
-MAX_ITER=100;
-theta=zeros(n_dic,ds,n_sub);
-thetaold=zeros(n_dic,ds,n_sub);
-thet1 =ones(n_dic,ds, n_sub)/ds ;
-thet2 =ones(n_dic,ds, n_sub)/ds ;
-m1 = ones(n_sub, ds)/ds;
-m2 = ones(n_sub, ds)/ds;
-b1=[];
-b2=[];
-for i=1:MAX_ITER
-    fprintf('%4s%4d\t%f\n','step:',i);
-    fprintf('\n');
-    dx2=0;
-    for t=1:n_dic
-        [thet1(t,:,:),m1,b1]=MTW( L, Y_DIC(:,t),M,thet1(t,:,:),thet2(t,:,:),b1,epsilon, gamma,MTW_lamb, MTW_mu,m1,n_sub);
-        [thet2(t,:,:),m2,b2]=MTW(L,-Y_DIC(:,t), M, thet2(t,:,:),thet1(t,:,:),b2,epsilon, gamma,MTW_lamb, MTW_mu,m2,n_sub);
-        theta(t,:,:) = thet1(t,:,:) - thet2(t,:,:);
-        dx2 =max(dx2,(norm(squeeze(theta(t,:,:)) - squeeze(thetaold(t,:,:)), 1)^ 2)/ (norm(squeeze(theta(t,:,:)), 1) ^2));
-        thetaold(t,:,:)=theta(t,:,:);
-    end
-    fprintf('%s%4d\n','theta_dx2:',dx2);
-    MTW_theta=theta;
-    for sub=1:n_sub
-        s_mtw=MTW_theta(:,:,sub)'*Dic*ratio;
-        MTW_s_group(sub,:,:)=s_mtw;
-        MTW_thr(1,sub)=ThresholdSelect(s_mtw, 'ifplot',0);
-        Y=EEGdata((sub-1)*dc+1:sub*dc,:)*ratio;
-        MTW_ev(1,sub)=1 - norm(Y - L*s_mtw,'fro')^2/norm(Y,'fro')^2;
-    end
-     if dx2<1e-4
-        break;
-    end
-end
-
 %%  WRA_MTSI
 MTWVSSI_barycenter=zeros(ds,15);
 sigma0=zeros(1,n_sub);
