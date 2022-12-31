3e2cb4c2baf59d8adbfcaa5e1147e08460ce83ba
ESI_code <118883154+ESIworkcode@users.noreply.github.com>
2022-12-31 11:18:26 +0800

    Update PatchGenerate3.m

--- support/PatchGenerate3.m
+++ support/PatchGenerate3.m
@@ -1,61 +1,27 @@
-function [Patch,Area1,Area2] = PatchGenerate3(ActiveVoxSeed,Patch,VertConn,VertArea,overlap_area,area2)
-overlap_area=overlap_area*1e-4;
-area2=area2*1e-4;
-Area1 = sum(VertArea(Patch));
-Area2=0;
-while Area1 <= overlap_area && Area1 <= area2
-    if Area1 <= overlap_area && Area1 <= area2
-        verts = find(max(VertConn(Patch, :), [], 1));
-        newverts = setdiff(verts, Patch);%新增点
-        over_verts=intersect(ActiveVoxSeed,newverts);%新增重叠点
-        if ~isempty(over_verts)
-            over_Nouter = union(Patch,over_verts);%重叠点
-        else
-            break;
-        end
-        Area1 = sum(VertArea(over_Nouter));
-    end
-    if Area1 > overlap_area || Area1 > area2
-        overlap_Ndiff =  setdiff(over_Nouter,Patch);%新增重叠点
-        for i = 1: numel(overlap_Ndiff)
-            Patch = union(Patch,overlap_Ndiff(i));
-            Area1 = sum(VertArea(Patch));
-            if Area1 > overlap_area || Area1 > area2
-                break;
-            end
-        end
-    else
-        Patch=over_Nouter;
-    end
-end
-Patch1=Patch;
-diff_area=area2-Area1;
-if diff_area<=0
-    return 
-end
-while Area2 <= diff_area
-    if Area2 <= diff_area
-        verts = find(max(VertConn(Patch, :), [], 1));
-        newverts = setdiff(verts, Patch);%新增点
-        diff_verts=setdiff(newverts,ActiveVoxSeed);%新增非重叠点
-        if ~isempty(diff_verts)
-            Nouter = union(Patch,diff_verts);%重叠点+非重叠点
-        else
-            break;
-        end
-        Area2 = sum(VertArea(setdiff(Nouter,Patch1)));
-    end
-    if Area2 > diff_area
-        diff_Ndiff =  diff_verts;%新增非重叠点
-        for i = 1: numel(diff_Ndiff)
-            Patch = union(Patch,diff_Ndiff(i));
-            Area2 =Area2+ sum(VertArea(diff_Ndiff(i)));
-            if Area2 > diff_area
-                break;
-            end
-        end
-    else
-        Patch=Nouter;
-    end
-end
+function Patch = PatchGenerate(Seed,VertConn,VertArea,AreaDef)
 
+Patch = Seed;
+Area = sum(VertArea(Patch));
+while Area <= AreaDef
+    if Area <= AreaDef
+        newverts = tess_scout_swell(Patch, VertConn);
+        if ~isempty(newverts)
+        Nouter = union(Patch,newverts);
+        else
+            break;
+        end
+        Area = sum(VertArea(Nouter));
+    end
+    if Area > AreaDef
+        Ndiff = setdiff(Nouter,Patch);
+        for i = 1: numel(Ndiff)
+             Patch = union(Patch,Ndiff(i));
+              Area = sum(VertArea(Patch));
+            if Area > AreaDef
+                break;
+             end
+        end
+    else
+         Patch = Nouter;
+    end  
+end
